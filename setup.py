8cfb0e6507bd9cf1789852dbb7ef50a331305ce2
Oscar Koeroo <okoeroo@gmail.com>
2011-06-27 12:01:50 +0200

    Minor adjustments

--- setup.py
+++ setup.py
@@ -4,10 +4,12 @@ from distutils.core import setup
 
 setup(
     name="arp-2-bgp",
-    version="0.1",
-    description="Based on Arp information announce routes to a particular host using BGP.",
+    version="0.0.1",
+    description="Based on Arp information announce routes to a particular host using BGP",
+    keywords = 'arista networks switch arp bgp BGP',
     author="Oscar Koeroo",
-    author_email="okeroo@nikhef.nl",
+    author_email="okoeroo@nikhef.nl",
     url="https://github.com/okoeroo/arp-2-bgp",
     packages=["arp2bgp"]
 )
+
