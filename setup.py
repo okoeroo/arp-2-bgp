dd2b30587d5e1a7a8af5a5c63269079d3c5fccf2
Oscar Koeroo <okoeroo@gmail.com>
2011-06-28 20:02:48 +0200

    Adding files, editing the README and more

--- setup.py
+++ setup.py
@@ -10,6 +10,12 @@ setup(
     author="Oscar Koeroo",
     author_email="okoeroo@nikhef.nl",
     url="https://github.com/okoeroo/arp-2-bgp",
-    packages=["arp2bgp"]
+    packages=["arp2bgp"],
+    data_files=[  ('/etc', ['arp2bgp/arp-2-bgp.conf']),
+                  ('/etc/cron.d', ['arp2bgp/arp-2-bgp.cron'])
+               ],
+    options = {'bdist_rpm':{'post_install' : 'package_scripts/post_install',
+                            'post_uninstall' : 'package_scripts/post_uninstall'}},
+    long_description=open('README').read()
 )
 
