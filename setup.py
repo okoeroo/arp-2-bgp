cbd86a1a772af5fb2fa56f2a6744d6758c04718a
Oscar Koeroo <okoeroo@gmail.com>
2011-06-30 21:04:40 +0200

    Path adjustments and now making Two RPMS. To make a plain RPM and Arista specific.

--- setup.py
+++ setup.py
@@ -2,21 +2,46 @@
 
 from distutils.core import setup
 
+
+name="arp-2-bgp"
+version="0.0.1"
+description="Based on Arp information announce routes to a particular host using BGP"
+keywords = 'arista networks switch arp bgp BGP'
+author="Oscar Koeroo"
+author_email="okoeroo@nikhef.nl"
+url="https://github.com/okoeroo/arp-2-bgp"
+data_files=[  ('/etc',           ['arp2bgp/arp-2-bgp.conf']),
+              ('/etc/cron.d',    ['arp2bgp/arp-2-bgp.cron']),
+              ('/usr/local/bin', ['arp2bgp/arp-2-bgp.py'])
+         ]
+options = {'bdist_rpm':{'post_install' : 'package_scripts/post_install',
+                      'post_uninstall' : 'package_scripts/post_uninstall'}}
+long_description=open('README').read()
+
+
 setup(
-    name="arp-2-bgp",
-    version="0.0.1",
-    description="Based on Arp information announce routes to a particular host using BGP",
-    keywords = 'arista networks switch arp bgp BGP',
-    author="Oscar Koeroo",
-    author_email="okoeroo@nikhef.nl",
-    url="https://github.com/okoeroo/arp-2-bgp",
-#    packages=["arp2bgp"],
-    data_files=[  ('/etc', ['arp2bgp/arp-2-bgp.conf']),
-                  ('/etc/cron.d', ['arp2bgp/arp-2-bgp.cron']),
-                  ('/usr/local/bin', ['arp2bgp/arp-2-bgp.py'])
-               ],
-    options = {'bdist_rpm':{'post_install' : 'package_scripts/post_install',
-                            'post_uninstall' : 'package_scripts/post_uninstall'}},
-    long_description=open('README').read()
+    name=name + "-generic",
+    version=version,
+    description=description,
+    keywords=keywords,
+    author=author,
+    author_email=author_email,
+    url=url,
+    data_files=data_files,
+    options=options,
+    long_description=long_description
+)
+
+setup(
+    name=name + "-arista",
+    version=version,
+    description=description,
+    keywords=keywords,
+    author=author,
+    author_email=author_email,
+    url=url,
+    data_files=data_files,
+    options=options,
+    long_description=long_description
 )
 
